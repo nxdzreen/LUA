710a759212964470082ef5e04a37e5ea41c8a57e
nXdzrXn'HP <34988963+nxdzreen@users.noreply.github.com>
2022-08-26 21:40:52 +0800

    basic operation, update guess random

---
+++ basicOperator.lua
@@ -0,0 +1,14 @@
+print("Basic Operator")
+
+x=1
+y=2
+
+print(x < y)		--print True
+print(x > y)		--print False
+
+
+first = "ABC"			--CONCAT with new input
+first = first .. "DEF"
+
+print(first)
+print(#first)			--Length of string
--- guessRandNum.lua
+++ guessRandNum.lua
@@ -2,7 +2,7 @@ print "Guess A Number"
 math.randomseed(os.time())
 
 --num =math.random()	-- between 0 and 1
-randNum =math.random(3)	-- between 1 and LIMIT
+randNum =math.random(10)	-- between 1 and LIMIT
 --num =math.random(10,100)	-- between BOTTOM and LIMIT
 
 print(randNum)
@@ -12,6 +12,12 @@ guess = io.read("*n")		--get the input from user
 print(guess)				--reprint the input from user
 
 
-if guess == randNum then
-print"Correct Guess"
+if guess < randNum then
+	print"Too Low"
+
+elseif guess > randNum then
+	print"Too High"
+
+elseif guess == randNum then
+	print"Correct Guess"
 end
