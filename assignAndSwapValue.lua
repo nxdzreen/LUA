a79b799ba08ef5498f0f2772c57944fa32356703
nXdzrXn'HP <34988963+nxdzreen@users.noreply.github.com>
2022-08-26 12:43:21 +0800

    Basic Syntax LUA

---
+++ assignAndSwapValue.lua
@@ -0,0 +1,11 @@
+A =5
+B =10
+
+print("Value for A is ",A)
+print("Value for B is ",B)
+
+A,B = B,A
+print("Done SWAP")
+
+print("Value for A is ",A)
+print("Value for B is ",B)
---
+++ guessRandNum.lua
@@ -0,0 +1,17 @@
+print "Guess A Number"
+math.randomseed(os.time())
+
+--num =math.random()	-- between 0 and 1
+randNum =math.random(3)	-- between 1 and LIMIT
+--num =math.random(10,100)	-- between BOTTOM and LIMIT
+
+print(randNum)
+
+
+guess = io.read("*n")		--get the input from user
+print(guess)				--reprint the input from user
+
+
+if guess == randNum then
+print"Correct Guess"
+end
